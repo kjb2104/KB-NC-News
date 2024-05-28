a0efdcbd76813fc6fc39942b637841b4a73e66a2
kjb2104 <160651849+kjb2104@users.noreply.github.com>
2024-05-28 14:14:15 +0100

    Revert "03 get api"

--- __tests__/endpoint/seed.test.js
+++ __tests__/endpoint/seed.test.js
@@ -20,33 +20,14 @@ describe("/api/topics", () => {
       .get("/api/topics")
       .expect(200)
       .then((response) => {
-        expect(response.body.topics).toHaveLength(3);
+        expect(response.body.topics).toHaveLength(3)
         response.body.topics.forEach((topic) => {
           expect(typeof topic.slug).toBe("string");
           expect(typeof topic.description).toBe("string");
         });
       });
   });
-});
 
-describe("/api", () => {
-  test("should GET: 200 responds with an object which describes all the endpoints on this API", () => {
-    return request(app)
-      .get("/api")
-      .expect(200)
-      .then((response) => {
-        const parsedResponse = JSON.parse(response.text);
-        const vals = Object.values(parsedResponse);
-        vals.forEach((obj) => {
-          expect(typeof obj.description).toBe("string");
-          expect(Array.isArray(obj.queries)).toBe(true);
-          expect(typeof obj.exampleResponse).toBe("object");
-        });
-      });
-  });
-});
-
-describe("/api/*", () => {
   test("should GET: 404 responds with an error message not found when passed a route that does not exist", () => {
     return request(app)
       .get("/api/topiczz")
--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-const fs = require('fs')
 const express = require('express')
 const app = express();
-const getTopics = require('./controllers/topics.controllers.js');
-const getApi = require('./controllers/api.controllers.js');
+const getTopics = require('./controllers/topics.controllers.js')
 app.use(express.json());
-
-
-app.get('/api', getApi)
 
 app.get('/api/topics', getTopics);
 
 
-    
+
 app.all('*', (req, res) => {
     res.status(404).send({msg: "Not Found"})
       })
