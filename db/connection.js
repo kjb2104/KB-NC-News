383f1e7b635b6555a828c42aae7eb5438b7bf928
kjb2104 <kjblacklock@LAPTOP-NB47FNHH.localdomain>
2024-05-30 15:32:44 +0100

    add listen.js and modifications to package.json

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+
 const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
@@ -6,8 +7,15 @@ require('dotenv').config({
 });
 
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set');
 }
 
-module.exports = new Pool();
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
